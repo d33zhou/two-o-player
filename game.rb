2ad4e46414cabbbce0262f4c3b0612dfdbd37257
d33zhou <d33zhou@outlook.com>
2022-02-01 23:35:50 +0000

    refactor code

--- game.rb
+++ game.rb
@@ -1,67 +1,26 @@
 class Game
 
   def initialize
-    @@players = []
-    @@current_player = ''
-    @@keep_playing = true
+    @players = []
+    @player1 = ''
+    @player2 = ''
+    @current_player = ''
+    @keep_playing = true
   end
 
-  def ask_question
-    puts "----- NEW TURN -----"
-
-    name = @@current_player.name
-    
-    numbers = generate_numbers
-    question = "What does #{numbers.first} plus #{numbers.last} equal?"
-
-    puts "#{name}: #{question}"
-
-    answer = gets.chomp
-    
-    if numbers.first + numbers.last == answer.to_i
-      puts "#{name}: YES! You are correct."
-      update_lives
-    else
-      puts "#{name}: Seriously? No!"
-      update_lives(true)
-    end
-
-    next_player
-  end
-  
   def add_player(player)
-    @@players << player
-  end
-
-  def next_player
-    if @@current_player == @@players.first
-      @@current_player = @@players.last
-    else
-      @@current_player = @@players.first
-    end
-  end
-
-
-  def update_lives(wrong_answer = false)
-    if wrong_answer
-      @@current_player.lives -= 1
-    end
-
-    puts "P1: #{@@players.first.lives}/3 vs P2: #{@@players.last.lives}/3"
-
-    if @@players.first.lives <= 0 || @@players.last.lives <= 0
-      @@keep_playing = false
-    end
+    @players << player
   end
 
   def play_game
-    if @@players.length == 2
+    if @players.length == 2
       puts "----- NEW GAME -----"
       puts "Starting a new game!"
       
-      @@current_player = @@players.first
+      @player1 = @current_player = @players.first
+      @player2 = @players.last
 
-      while @@keep_playing do
+      while @keep_playing do
         ask_question
       end
 
@@ -82,13 +41,56 @@ class Game
     return numbers
   end
 
+  def ask_question
+    puts "----- NEW TURN -----"
+
+    name = @current_player.name
+    
+    numbers = generate_numbers
+    question = "What does #{numbers.first} plus #{numbers.last} equal?"
+
+    puts "#{name}: #{question}"
+
+    answer = gets.chomp
+    
+    if numbers.first + numbers.last == answer.to_i
+      puts "#{name}: YES! You are correct."
+      update_lives
+    else
+      puts "#{name}: Seriously? No!"
+      update_lives(true)
+    end
+
+    next_player
+  end
+
+  def update_lives(wrong_answer = false)
+    if wrong_answer
+      @current_player.lives -= 1
+    end
+
+    puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
+
+    if @player1.lives <= 0 || @player2.lives <= 0
+      @keep_playing = false
+    end
+  end
+
+  def next_player
+    if @current_player == @player1
+      @current_player = @player2
+    else
+      @current_player = @player1
+    end
+  end
+
   def end_game
     puts "----- GAME OVER -----"
     
-    if @@players.first.lives == 0
-      puts "#{@@players.last.name} wins with a score of #{@@players.last.lives}/3"
+    if @player1.lives == 0
+      puts "#{@player2.name} wins with a score of #{@player2.lives}/3"
     else
-      puts "#{@@players.first.name} wins with a score of #{@@players.first.lives}/3"
+      puts "#{@player1.name} wins with a score of #{@player1.lives}/3"
     end
 
     puts "Good bye!"
--- main.rb
+++ main.rb
@@ -2,8 +2,8 @@ require './game'
 require './player'
 
 game = Game.new
-player1 = Player.new('Joe')
-player2 = Player.new('Bob')
+player1 = Player.new('Player 1')
+player2 = Player.new('Player 2')
 game.add_player(player1)
 game.add_player(player2)
 
